aa013e4f7900a9b056aca6d4d577069758dc6883
Justin Peczenij <justinpeczenij@gmail.com>
2021-05-06 12:53:21 -0400

    server running

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log('it is working')
+const express = require('express')
+
+const server = express()
+server.use(express.json())
+
+server.get('/api/*', (req, res) => {
+    res.json({
+        cohort: 'Web 41',
+    })
+})
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, ()=> {
+    console.log(`listening on ${PORT}`)
+})
