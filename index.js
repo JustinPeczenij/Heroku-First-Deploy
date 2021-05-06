77429ca7256f844f66170081b1c7e0e23c78be72
Justin Peczenij <justinpeczenij@gmail.com>
2021-05-06 13:55:54 -0400

    wip

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 require('dotenv').config()
+//ADD STUFF FOR FRONT END TO RENDER ON HEROKU
+const path = require('path') //this is a node thing
+
 console.log('it is working')
+
 const express = require('express')
 
 const server = express()
+
+//RENDER FRONTEND ON HEROKU
+server.use(express.static(path.join(__dirname, 'client/build'))) //node thing
+
 server.use(express.json())
+
+const PORT = process.env.PORT 
 
 server.get('/api/*', (req, res) => {
     res.json({
@@ -11,7 +21,9 @@ server.get('/api/*', (req, res) => {
     })
 })
 
-const PORT = process.env.PORT || 5000
+server.use('*', (_, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html')) //render REACT APP, which is the index.html
+})
 
 server.listen(PORT, ()=> {
     console.log(`listening on ${PORT}`)
